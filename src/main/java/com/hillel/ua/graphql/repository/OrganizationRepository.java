69388c871b13f2ab2889ca1533f0d351942c8645
Anna Padun <roananik1988@gmail.com>
2024-07-23 18:07:02 +0200

    HW-11 create docker compose/ create Dockerfile/ add method update delete to classes

--- src/main/java/com/hillel/ua/graphql/repository/OrganizationRepository.java
+++ src/main/java/com/hillel/ua/graphql/repository/OrganizationRepository.java
@@ -1,10 +1,17 @@
 package com.hillel.ua.graphql.repository;
 
 import com.hillel.ua.graphql.entities.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface OrganizationRepository extends
         CrudRepository<Organization, Integer>, JpaSpecificationExecutor<Organization> {
+    @Query("select o from Organization o where o.name = :name")
+    Optional<Organization> findByName(@Param("name") String name);
 }
 
